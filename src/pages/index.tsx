241399cbeca8473a014fa62a411a536db3d4a3d6
unknown <khalilbohner@gmail.com>
2022-09-09 23:08:10 -0300

    :maple_leaf: Dynamic product fetching with the stripe sdk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,60 @@
-import { Container, Product, ProductFooter } from '../styles/pages/home';
+import { HomeContainer, Product, ProductFooter } from '../styles/pages/home';
 import shirtPath from '../assets/Shirt.png';
 import Image from 'next/future/image';
+import { GetStaticProps } from 'next';
+import { stripeAPI } from '../services/stripe';
+import { Stripe } from 'stripe';
+import { NumberFormatter } from '../utils/formatter';
+
+interface IHomeProps {
+	products: {
+		id: string;
+		name: string;
+		imageUrl: string;
+		price: string;
+	}[];
+}
 
 // implement carousel with radix ui when it releases
-export default function Home() {
+export default function Home({ products }: IHomeProps) {
 	return (
-		<Container>
-			<Product>
-				<Image src={shirtPath} alt="" />
+		<HomeContainer>
+			{products.map(product => {
+				return (
+					<Product key={product.id}>
+						<Image src={product.imageUrl} alt="" />
 
-				<ProductFooter>
-					<span>Camisa</span>
+						<ProductFooter>
+							<span>{product.name}</span>
 
-					<strong>79,80</strong>
-				</ProductFooter>
-			</Product>
-			<Product>
-				<Image src={shirtPath} alt="" />
-
-				<ProductFooter>
-					<span>Camisa</span>
-
-					<strong>79,80</strong>
-				</ProductFooter>
-			</Product>
-			<Product>
-				<Image src={shirtPath} alt="" />
-
-				<ProductFooter>
-					<span>Camisa</span>
-
-					<strong>79,80</strong>
-				</ProductFooter>
-			</Product>
-		</Container>
+							<strong>{product.price}</strong>
+						</ProductFooter>
+					</Product>
+				);
+			})}
+		</HomeContainer>
 	);
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+	const response = await stripeAPI.products.list({
+		expand: ['data.default_price'],
+	});
+
+	const products = response.data.map(product => {
+		const price = product.default_price as Stripe.Price;
+
+		return {
+			id: product.id,
+			name: product.name,
+			imageUrl: product.images[0],
+			price: NumberFormatter.format(price.unit_amount! / 100),
+		};
+	});
+
+	return {
+		props: {
+			products,
+		},
+	};
+};
--- src/styles/pages/home/index.ts
+++ src/styles/pages/home/index.ts
@@ -1,6 +1,6 @@
 import { styled } from '../..';
 
-export const Container = styled('div', {
+export const HomeContainer = styled('div', {
 	width: '100%',
 	maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
 	minHeight: 656,
